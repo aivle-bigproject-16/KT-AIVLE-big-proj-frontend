58f8f9dfec7fd01ec429de149e4dacaa7c1c2a11
Yonhwa-Kondok <Yonhwa.Kondok@gmail.com>
2026-07-14 16:16:20 +0900

    feat: 배터리 목록의 아이콘 컴포넌트 분리

--- src/features/battery/components/BatteryList.tsx
+++ src/features/battery/components/BatteryList.tsx
@@ -1,66 +1,17 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './BatteryList.css'
+import {
+  DownloadIcon,
+  FilterIcon,
+  CalendarIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+  ChevronLeftIcon,
+  ChevronRightSmallIcon,
+} from './BatteryListIcons'
 import { useBatteryListStore } from '../store/useBatteryListStore'
 import { ROUTES } from '@/core/navigation/routes'
-
-function DownloadIcon() {
-  return (
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M12 3v12M7 10l5 5 5-5" />
-      <path d="M4 19h16" />
-    </svg>
-  )
-}
-
-function FilterIcon() {
-  return (
-    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M4 5h16l-6 8v6l-4-2v-4L4 5Z" />
-    </svg>
-  )
-}
-
-function CalendarIcon() {
-  return (
-    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <rect x="3" y="5" width="18" height="16" rx="2" />
-      <path d="M3 10h18M8 3v4M16 3v4" />
-    </svg>
-  )
-}
-
-function ChevronDownIcon() {
-  return (
-    <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M6 9l6 6 6-6" />
-    </svg>
-  )
-}
-
-function ChevronRightIcon() {
-  return (
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M9 6l6 6-6 6" />
-    </svg>
-  )
-}
-
-function ChevronLeftIcon() {
-  return (
-    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M15 6l-6 6 6 6" />
-    </svg>
-  )
-}
-
-function ChevronRightSmallIcon() {
-  return (
-    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M9 6l6 6-6 6" />
-    </svg>
-  )
-}
 
 function formatDateTime(value: string | null): string {
   if (!value) return '-'
